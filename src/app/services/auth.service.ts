0c7555df36304e8a4f01969a9362aa602753d13f
Kyle <kyleryan480@gmail.com>
2018-09-15 22:05:38 -0700

    using ngIf with auth status to render conditionally

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { JwtHelper, tokenNotExpired } from "angular2-jwt";
 import { Injectable } from "@angular/core";
 import { Http } from "@angular/http";
 import "rxjs/add/operator/map";
@@ -25,6 +26,27 @@ export class AuthService {
   }
 
   isLoggedIn() {
-    return false;
+    // - This -
+    return tokenNotExpired();
+
+    // - Does all of... -
+
+    // - This -
+    // let jwtHelper = new JwtHelper();
+    // // Retrieving token from local storage.
+    // let token = localStorage.getItem("token");
+    // if (!token) {
+    //   return false;
+    // } else {
+    //   // Collecting expiration date of said token.
+    //   let expirationDate = jwtHelper.getTokenExpirationDate(token);
+    //   // Determine whether or not the token is expired.
+    //   let isExpired = jwtHelper.isTokenExpired(token);
+
+    //   // console.log("Expiration", expirationDate);
+    //   // console.log("isExpired", isExpired);
+
+    //   return !isExpired;
+    // }
   }
 }
